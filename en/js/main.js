355af6cd70bbdcc0ee7b4342e215fd327019d12e
VickyCR <vickycrweb@gmail.com>
2017-09-18 15:59:39 -0500

    Create main.js

---
+++ en/js/main.js
@@ -0,0 +1,12 @@
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDFPaQhc4qfMsVSUEkbzU33D2fK0FFgAzQ",
+    authDomain: "groceries-budget.firebaseapp.com",
+    databaseURL: "https://groceries-budget.firebaseio.com",
+    projectId: "groceries-budget",
+    storageBucket: "groceries-budget.appspot.com",
+    messagingSenderId: "216666611946"
+  };
+  firebase.initializeApp(config);
