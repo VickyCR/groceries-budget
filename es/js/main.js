4c453eddc7f3f177049833f96de12cdb2a2fbc48
VickyCR <vickycrweb@gmail.com>
2017-09-21 20:31:28 -0500

    Update to local files

--- es/js/main.js
+++ es/js/main.js
@@ -1,9 +1,7 @@
 function main() {
   "use strict";
-
-  
-  // Initialize Firebase
-  var config = {
+  var database;
+  var config = {
     apiKey: "AIzaSyDFPaQhc4qfMsVSUEkbzU33D2fK0FFgAzQ",
     authDomain: "groceries-budget.firebaseapp.com",
     databaseURL: "https://groceries-budget.firebaseio.com",
@@ -12,10 +10,47 @@ function main() {
     messagingSenderId: "216666611946"
   };
   firebase.initializeApp(config);
+  database = firebase.database();
   
-  $('#btnAddArticle').click(function(){
-    var article = txtArticle.value;
-    var category = txtCategory;
+  var ref = database.ref('es/frutasVegetales');
+  ref.on('value', gotData, errData);
+  
+  function gotData(data) {
+    var articles = data.val();
+    var keys = Object.keys(articles);
+    console.log(keys);
+    for (var i = 0; i < keys.length; i++) {
+      var k = keys[i];
+      var article = articles[k].Article;
+      var uprice = articles[k].UnitPrice;
+      console.log(article,uprice,k,articles);
+    }
+  }
+  
+  function errData(err) {
+    console.log('Error');
+    console.log(err);
+  }
+  
+  
+  $('#btnAddArticle').on('click', function(){
+    var article = $('#txtArticle').val(),
+        category = $('#txtCategory').val(),
+        unitPrice = $('#nmbrUnitPrice').val(),
+        unitName = $('#txtUnitName').val(),
+        recommended = $('#nmbrRecommended').val(),
+        ref = database.ref('es/' + category);
+    
+    var data = {
+      RecommendedCuantity: recommended,
+      UnitName: unitName,
+      UnitPrice: unitPrice,
+      Article: article
+    }
+    
+    var result = ref.push(data);
+    console.log(result.key);
+    
   });
  }
 $(document).ready(main);
