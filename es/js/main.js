10e81c3ad3a5953cf8fef3e6d8e4e6e7ca6dbd39
VickyCR <vickycr@MacNolia.local>
2017-10-02 00:14:55 -0500

    Articles added

--- es/js/main.js
+++ es/js/main.js
@@ -14,49 +14,83 @@ function main() {
   
   database = firebase.database();
   
-  
-  var ref = database.ref('es/frutasVegetales');
-  ref.on('value', gotData, errData);
-  
-  function gotData(data) {
-    var articles = data.val();
-    var keys = Object.keys(articles);
-    console.log(keys);
-    $('articles-list').remove();
-    for (var i = 0; i < keys.length; i++) {
-      var k = keys[i];
-      var article = articles[k].Article;
-      var uprice = articles[k].UnitPrice;
-      var recommended = articles[k].RecommendedCuantity;
-      //console.log(article,uprice,k,articles);
-      $('.articles-list').append('<tr><td>' + article + '</td><td>' + uprice + '</td><td>' + recommended + '</td></tr>');
-    }
-  }
-  
-  function errData(err) {
-    console.log('Error');
-    console.log(err);
-  }
-  
-  
   $('#btnAddArticle').on('click', function(){
     var article = $('#txtArticle').val(),
         category = $('#txtCategory').val(),
         unitPrice = $('#nmbrUnitPrice').val(),
         unitName = $('#txtUnitName').val(),
-        recommended = $('#nmbrRecommended').val(),
-        ref = database.ref('es/' + category);
+        recommended = $('#nmbrRecommended').val();
+    
+    $('.articles-list').empty();
+    
+    var refAdd = database.ref('es/' + category);
     
     var data = {
       RecommendedCuantity: recommended,
       UnitName: unitName,
       UnitPrice: unitPrice,
-      Article: article
+      Article: article,
+      Category: category
     }
-    
-    var result = ref.push(data);
-    console.log(result.key);
+    refAdd.push(data);
+    //var result = refAdd.push(data);
+    console.log('Acabo de subir data');
+    //console.log(result.key);
+    //FIXME Se está llamando primero a gotData por alguna razón... averiguar y corregir esta situación insportable... aaaaaah T_T
     
   });
+  
+  $('#btnSearch').on('click', function() {
+    //TODO search queries
+  });
+  
+  var ref = database.ref('es');
+  ref.on('value', gotCategory);
+  
  }
 $(document).ready(main);
+
+function gotCategory(data) {
+  var departments = data.val();
+  var categories = Object.keys(departments);
+  //console.log('departments var');
+  //console.log(departments);
+  //console.log('categories var');
+  //console.log(categories);
+  console.log('Estoy obteniendo la categoría')
+  for (var i = 0; i < categories.length; i++) {
+    console.log('hola ',i);
+    console.log('Estoy enviando la categoría', categories[i]);
+    //console.log(categories[i]);
+    var database = firebase.database();
+    var refCategory = database.ref('es/' + categories[i]);
+    refCategory.on('value', gotData, errData);
+    refCategory.off('value', gotData);
+  }
+}
+
+function gotData(data) {
+  var articles = data.val();
+  var keys = Object.keys(articles);
+  //console.log('articles var');
+  //console.log(articles);
+  //console.log('keys var');
+  //console.log(keys);
+  console.log('Recibí categoría y voy a imprimir los artículos')
+  for (var i = 0; i < keys.length; i++) {
+    var k = keys[i];
+    var category = articles[k].Category;
+    var article = articles[k].Article;
+    var unitPrice = articles[k].UnitPrice;
+    var unitName = articles[k].UnitName;
+    var recommended = articles[k].RecommendedCuantity;
+    //console.log(k);
+    console.log('Estoy analizando un artículo')
+    $('.articles-list').append('<tr><td>' + category + '</td><td>' + article + '</td><td>' + unitPrice + ' / ' + unitName + '</td><td>' + recommended + '</td></tr>');
+  }
+}
+
+function errData(err) {
+  console.log('Error');
+  console.log(err);
+}
